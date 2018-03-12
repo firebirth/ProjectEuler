625b64ef4b8b7b5ef146968857f76c8bc3f7aa91
Maksym Olhovych <maksym.olkhovych@gmail.com>
2018-03-12 21:31:12 +0200

    Problem 1

---
+++ ProjectEuler.Tasks/Problem1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace ProjectEuler.Tasks
+{
+    public static class Problem1
+    {
+        public static long GetSumBelow1000()
+        {
+            var numbers = Enumerable.Range(1, 999).Select(i => (long) i).ToArray();
+
+            var multiplesOf3 = numbers.Where(i => i % 3 == 0);
+            var multiplesOf5 = numbers.Where(i => i % 5 == 0);
+
+            return multiplesOf3.Concat(multiplesOf5).Distinct().Sum();
+        }
+    }
+}
---
+++ ProjectEuler/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using ProjectEuler.Tasks;
+
+namespace ProjectEuler
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Out.WriteLine(Problem1.GetSumBelow1000());
+        }
+    }
+}
